713d0443b19317ad1d1a97d6e27173811f44213c
samuel-mr <sam_chacla@hotmail.com>
2020-03-03 10:47:39 -0500

    Mensajes + Listas

---
+++ ControlesBasicos/Client/HelperJS/IInteractionMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlesBasicos.Client.HelperJS
+{
+    public interface IInteractionMessage
+  {
+    Task ShowErrorMessage(string mensaje);
+    Task ShowErrorMessage(string title, string mensaje);
+    Task ShowOkMessage(string mensaje);
+    Task ShowOkMessage(string title, string mensaje);
+  }
+}
---
+++ ControlesBasicos/Client/HelperJS/SweetAlertMessage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlesBasicos.Client.HelperJS
+{
+  public class SweetAlertMessage : IInteractionMessage
+  {
+    private readonly IJSRuntime js;
+
+    public SweetAlertMessage(IJSRuntime js)
+    {
+      this.js = js;
+    }
+    public async Task ShowErrorMessage(string mensaje)
+    {
+      await ShowErrorMessage("Error", mensaje);
+    }
+    public async Task ShowErrorMessage(string title, string mensaje)
+    {
+      await ShowMessage(title, mensaje, SweetAlertIcon.error);
+    }
+    public async Task ShowOkMessage(string mensaje)
+    {
+      await ShowOkMessage("Exitoso", mensaje);
+    }
+    public async Task ShowOkMessage(string title, string mensaje)
+    {
+      await ShowMessage(title, mensaje, SweetAlertIcon.success);
+    }
+
+    private async ValueTask ShowMessage(string titulo, string mensaje, SweetAlertIcon tipo)
+    {
+      await js.InvokeVoidAsync("Swal.fire", titulo, mensaje, Enum.GetName(typeof(SweetAlertIcon), tipo));
+    }
+
+    public enum SweetAlertIcon
+    {
+      warning,
+      error,
+      success,
+      info,
+      question
+    }
+  }
+}
