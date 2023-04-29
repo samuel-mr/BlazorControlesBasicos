90cc67a75e4e9703444d16b25523daca31d65ea8
samuel-mr <sam_chacla@hotmail.com>
2023-04-29 09:11:27 -0500

    Added databinding examples

---
+++ ControlesBasicos/Client/Pages/DataBinding/TreeClass/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlesBasicos.Client.Pages.DataBinding.TreeClass
+{
+  public class Book : IEquatable<Book>
+  {
+
+    private string title = "";
+    public string Title
+    {
+      get
+      {
+        return title;
+      }
+      set
+      {
+        if (title != value)
+        {
+          title = value;
+        }
+      }
+    }
+
+    private int _year;
+    public int Year
+    {
+      get
+      {
+        return _year;
+      }
+      set
+      {
+        if (_year != value)
+        {
+          _year = value;
+        }
+      }
+    }
+
+    public bool Equals(Book other)
+    {
+      if (other == null)
+        return false;
+      if (this.Title == other.Title && this.Year == other.Year)
+        return true;
+
+      return false;
+    }
+  }
+}
